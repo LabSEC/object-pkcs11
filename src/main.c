5b0dff4dadc24666c1a632f904823d9ad7926364
Lucas Perin <lucas.perin@posgrad.ufsc.br>
2015-10-26 17:28:26 -0200

    Main function with simple print

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 
+#include <stdio.h>
 
 
 int main ( int argc, const char* argv[] )
 {
+	printf("Hello World\n");
 
+	for( int i = 0; i < argc; i++ )
+	{
+		printf( "arg %d: %s\n", i, argv[i] );
+	}
 
 }
